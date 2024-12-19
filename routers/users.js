4bef40cb573bd1e1775fc168182950d8cbe3c7fd
Ahmed Shahid <a.razashahid19@gmail.com>
2024-12-19 15:28:24 +0500

    User route for fetching user info from db

--- routers/users.js
+++ routers/users.js
@@ -1,5 +1,21 @@
 import express from "express";
+import { authenticateUser } from "../middleware/authenticate";
+import { userModel } from "../models/user";
+import sendResponse from "../helpers/sendResponse";
 
 const router = express.Router();
+
+router.get("/info", authenticateUser, async (req, res) => {
+  try {
+    const user = await userModel.findById(req.user._id).exec(true);
+
+    if (!user) return sendResponse(res, 404, null, false, "User not found.");
+
+    return sendResponse(res, 200, user, true, "User data fetched from db.");
+  } catch (err) {
+    console.log("Internal server error ======>>>>>>", err);
+    return sendResponse(res, 500, null, false, "Internal server error.");
+  }
+});
 
 export default router;
