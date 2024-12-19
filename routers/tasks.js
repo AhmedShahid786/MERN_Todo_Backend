a788ead954b0cf5657e414cfdc9c718abea1f9bc
Ahmed Shahid <a.razashahid19@gmail.com>
2024-12-19 16:16:26 +0500

    Task route for adding and fetching tasks

--- routers/tasks.js
+++ routers/tasks.js
@@ -1,5 +1,42 @@
 import express from "express";
+import { taskModel } from "../models/task.js";
+import { authenticateUser } from "../middleware/authenticate.js";
+import sendResponse from "../helpers/sendResponse.js";
 
 const router = express.Router();
+
+router.get("/", authenticateUser, async (req, res) => {
+  try {
+    let tasks = await taskModel.find({ createdBy: req.user._id });
+
+    if (!tasks) return sendResponse(res, 404, null, false, "Tasks not found.");
+
+    return sendResponse(res, 200, tasks, true, "Tasks fetched successfully.");
+  } catch (err) {
+    console.log("Internal server error ======>>>>>>", err);
+    return sendResponse(res, 500, null, false, "Internal server error.");
+  }
+});
+
+router.post("/", authenticateUser, async (req, res) => {
+  try {
+    const { title, description } = req.body;
+
+    if (!title)
+      return sendResponse(res, 403, null, false, "Title field in required.");
+
+    let newTask = new taskModel({
+      title,
+      description: description ? description : null,
+      createdBy: req.user._id,
+    });
+
+    newTask = await newTask.save();
+    sendResponse(res, 201, newTask, true, "Task added successfully.");
+  } catch (err) {
+    console.log("Internal server error ======>>>>>>", err);
+    return sendResponse(res, 500, null, false, "Internal server error.");
+  }
+});
 
 export default router;
