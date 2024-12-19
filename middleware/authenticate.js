01f268ce6f3a0b952872ede3a7d75ae15ae2e968
Ahmed Shahid <a.razashahid19@gmail.com>
2024-12-19 15:28:03 +0500

    Authentication middleware with jwt token

---
+++ middleware/authenticate.js
@@ -0,0 +1,29 @@
+import jwt from "jsonwebtoken";
+import "dotenv/config";
+import sendResponse from "../helpers/sendResponse";
+import { userModel } from "../models/user";
+
+export async function authenticateUser(req, res, next) {
+  try {
+    const bearerToken = req?.headers?.authorization;
+    const token = bearerToken.split(" ")[1];
+
+    if (!token)
+      return sendResponse(res, 403, null, false, "Token not provided.");
+
+    const decodedData = jwt.verify(token, process.env.merntodoapp);
+    console.log("Decoded data from jwt ======>>>>>>", decodedData);
+
+    if (!decodedData)
+      return sendResponse(res, 404, null, false, "Error in token decoding.");
+
+    const user = await userModel.findById(decodedData._id);
+    if (!user) return sendResponse(res, 404, null, false, "User not found.");
+
+    req.user = decodedData;
+    next();
+  } catch (err) {
+    console.log("Internal server error ======>>>>>>", err);
+    return sendResponse(res, 500, null, false, "Internal server error.");
+  }
+}
